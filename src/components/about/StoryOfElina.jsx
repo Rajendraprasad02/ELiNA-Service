66cf9208765823d66d2452ee20d224e530d7961d
Rajendraprasad02 <rajendra.prasad@talentakeaways.com>
2024-07-11 18:55:38 +0530

    11/07/2024

--- src/components/about/StoryOfElina.jsx
+++ src/components/about/StoryOfElina.jsx
@@ -6,13 +6,13 @@ const StoryOfElina = ({ storyOfElina }) => {
       <div className="">
         {storyOfElina.map((item) => (
           <div
-            className={` lg:h-screen flex flex-col lg:flex-row ${
+            className={`p-16  lg:h-screen flex flex-col lg:flex-row ${
               item.ImgDirection === "left" ? "" : "lg:flex-row-reverse"
             } justify-center gap-10 md:gap-[5%] px-8  md:px-20 items-center ${
               item.ImgDirection === "left" ? "bg-[#f0f2fc]" : ""
             }`}
           >
-            <div className="w-[100%] md:w-[50%] m-16">
+            <div className="w-[100%] md:w-[50%] ">
               <img
                 className={`${
                   item.ImgDirection === "left" ? "items-end" : "items-end"
